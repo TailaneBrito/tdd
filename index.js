659a4ee2c88df5cf36239a1d54f5189364fff779
Brito, Tailane <tailane.brito01@gmail.com>
2020-12-14 19:32:20 -0300

    First test green

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+function fahreheint2celsius(fahreheint){
+	return 100;
+}
+
 describe.only('the fahrenheit to celsius canary spec', () => {
-   it('shows the infrastructure works', () => {
-      true.should.be.true();	   
-   });
+	it('shows the infrastructure works', () => {
+		true.should.be.true();	   
+	});
 
 describe('fahrenheit to celsius converter behaves as follows', () => {
 	it('212 degress fahrehit is 100 degrees celsius', () => {
@@ -10,6 +14,11 @@ describe('fahrenheit to celsius converter behaves as follows', () => {
 	it('32 degrees fahrenheit is 0 degrees celsius');
 	it('50 degrees fahreheint is 10 degress celsius');
 });
-  
+	//error message should drive what we do. no more writting code to resolve the issue.
+	// four rules for simple design.
+	// 1 pass the tast (green bar tells us),
+	// 2 review intention
+	// 3 no duplication
+	// 4 fewest number of elements.
 });
 	
