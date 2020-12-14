ae2c4eb58e764252ec852c637ff7fb3bbe5a35a2
Brito, Tailane <tailane.brito01@gmail.com>
2020-12-14 18:54:49 -0300

    Create the test suite

---
+++ index.js
@@ -0,0 +1,13 @@
+describe.only('the fahrenheit to celsius canary spec', () => {
+   it('shows the infrastructure works', () => {
+      true.should.be.true();	   
+   });
+
+describe('fahrenheit to celsius converter behaves as follows', () => {
+	it('212 degress fahrehit is 100 degrees celsius');
+	it('32 degrees fahrenheit is 0 degrees celsius');
+	it('50 degrees fahreheint is 10 degress celsius');
+});
+  
+)};
+	
